7fe702ca43003f7694855b4f2bdbabb916bd58a6
Merwan Achibet <achibet.merwan@gmail.com>
2015-08-22 23:50:02 +0200

    Fix jump glitches

--- main.js
+++ main.js
@@ -6,10 +6,7 @@ var C_NONE             = 0,
     C_BOUNDING_BOX     = 1 << 1,
     C_RENDERABLE       = 1 << 2,
     C_INPUT            = 1 << 3,
-    C_PHYSICS          = 1 << 4,
-    C_MOVE             = 1 << 5,
-    C_JUMP             = 1 << 6,
-    C_DOUBLE_JUMP      = 1 << 7
+    C_PHYSICS          = 1 << 4
 
 var world = {
   mask: [],
@@ -59,8 +56,6 @@ function createMunster(position) {
     | C_RENDERABLE
     | C_INPUT
     | C_PHYSICS
-    | C_MOVE
-    | C_JUMP
 
   world.position[e] = point(position.x, position.y)
   world.renderable[e] = renderMunster
@@ -120,9 +115,13 @@ function initPhysics() {
   engine = Matter.Engine.create()
   engine.world.gravity.y = 0.7
 
+  // The ground must be touched before jumps
   Matter.Events.on(engine, 'collisionActive', function touchedGround(event) {
-    if (event.pairs[0].bodyA === world.body[munster] && jumping) {
-      jumping = doubleJumping = false;
+    var pair = event.pairs[0]
+
+    if (pair.bodyA === world.body[munster] && jumping) {
+      if (pair.collision.normal.y < -0.9)
+        jumping = doubleJumping = false
     }
   })
 }
@@ -130,22 +129,27 @@ function initPhysics() {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Input
 
-var keys = {}
-function onKeyDown(e) { keys[e.which] = true }
-function onKeyUp(e) { keys[e.which] = false }
-
 // Keyboard keycodes
 var K_LEFT  = 37,
     K_RIGHT = 39,
     K_SPACE = 32
 
+var keys = {}
+var justChanged = {} // Keep track of which keys have just been pressed/released
+
 function initKeyListeners() {
-  window.addEventListener('keydown', onKeyDown)
-  window.addEventListener('keyup', onKeyUp)
+  window.addEventListener('keydown', function(e) {
+    justChanged[e.which] = keys[e.which] === false
+    keys[e.which] = true
+  })
+  window.addEventListener('keyup', function(e) {
+    justChanged[e.which] = keys[e.which] === true
+    keys[e.which] = false
+  })
 }
 
-var velocity = 0.02
-var jumpVelocity = 0.3
+var velocity = 0.01
+var jumpVelocity = 0.25
 var jumping = false
 var doubleJumping = false
 
@@ -161,23 +165,29 @@ function controls() {
   // Horizontal moves
   if (keys[K_LEFT])
     applyForce(body, point(-velocity * multiplier, 0))
-
   if (keys[K_RIGHT])
     applyForce(body, point(velocity * multiplier, 0))
 
-  // Jump
-  if (keys[K_SPACE] && jumping === false ) {
-    applyForce(body, point(0, -jumpVelocity))
-    jumping = true
+  // If the jump key was just pressed
+  if (keys[K_SPACE] && justChanged[K_SPACE]) {
+
+    // Jump
+    if (jumping === false ) {
+      applyForce(body, point(0, -jumpVelocity))
+      jumping = true
+      sfx_play('sfx-jump')
+    }
+
+    // Double jump!
+    else if (jumping === true && doubleJumping === false && body.velocity.y > 0) {
+      applyForce(body, point(0, -jumpVelocity))
+      doubleJumping = true;
+    }
   }
 
-  // Double jump!
-  else if (keys[K_SPACE] && jumping === true && doubleJumping === false && body.velocity.y > 0) {
-    applyForce(body, point(0, -jumpVelocity))
-    doubleJumping = true;
-  }
+  // Clear
+  justChanged = {}
 }
-
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Audio
